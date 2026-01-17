6f3624b22a624e877b3684c5a37569711fb57719
ayejay-nz <55215693+ayejay-nz@users.noreply.github.com>
2026-01-17 13:09:18 +1300

    Skip duplicate inserts into `listening_history` table

--- server/src/db/queries/listeningHistory.ts
+++ server/src/db/queries/listeningHistory.ts
@@ -3,5 +3,9 @@ import { ListeningHistory } from '../types';
 import { Insertable } from 'kysely';
 
 export async function addListeningHistory(history: Insertable<ListeningHistory>[]) {
-    return await db.insertInto('listeningHistory').values(history).execute();
+    return await db
+        .insertInto('listeningHistory')
+        .values(history)
+        .onConflict((oc) => oc.constraint('listening_history_unique_play').doNothing())
+        .execute();
 }
