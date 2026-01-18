490e45ec4044fca034d1e96b689a09da15042054
ayejay-nz <55215693+ayejay-nz@users.noreply.github.com>
2026-01-19 04:55:05 +1300

    Add queries on table `artists` for track ingestion

---
+++ server/src/db/queries/artists.ts
@@ -0,0 +1,29 @@
+import { db } from '../index';
+import { Insertable } from 'kysely';
+import { Artists } from '../types';
+
+export async function getArtistIdsBySpotifyIds(spotifyIds: string[]) {
+    if (spotifyIds.length === 0) {
+        return new Map<string, number>();
+    }
+
+    const rows = await db
+        .selectFrom('artists')
+        .select(['artistId', 'spotifyId'])
+        .where('spotifyId', 'in', spotifyIds)
+        .execute();
+
+    return new Map(rows.map((row) => [row.spotifyId, row.artistId]));
+}
+
+export async function addArists(artists: Insertable<Artists>[]) {
+    if (artists.length === 0) {
+        return;
+    }
+
+    await db
+        .insertInto('artists')
+        .values(artists)
+        .onConflict((oc) => oc.column('spotifyId').doNothing())
+        .execute();
+}
