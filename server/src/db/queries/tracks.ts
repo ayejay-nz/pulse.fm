e35eaa1c664a4007cc59890f3b3582ccf041e1c3
ayejay-nz <55215693+ayejay-nz@users.noreply.github.com>
2026-01-19 04:34:29 +1300

    Add queries on table `tracks` for track ingestion

---
+++ server/src/db/queries/tracks.ts
@@ -0,0 +1,31 @@
+import { db } from '../index';
+import { Insertable } from 'kysely';
+import { Tracks } from '../types';
+
+export async function getTrackIdsFromSpotifyIds(spotifyIds: string[]) {
+    if (spotifyIds.length === 0) {
+        return new Map<string, number>();
+    }
+
+    const rows = await db
+        .selectFrom('tracks')
+        .select(['trackId', 'spotifyId'])
+        .where('spotifyId', 'in', spotifyIds)
+        .execute();
+
+    return new Map(
+        rows.filter((row) => row.spotifyId).map((row) => [row.spotifyId as string, row.trackId]),
+    );
+}
+
+export async function addTracks(tracks: Insertable<Tracks>[]) {
+    if (tracks.length === 0) {
+        return;
+    }
+
+    await db
+        .insertInto('tracks')
+        .values(tracks)
+        .onConflict((oc) => oc.column('spotifyId').doNothing())
+        .execute();
+}
